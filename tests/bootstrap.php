9f2d761e7ac1d89a6752a18036a597fa9ce2d8c7
Jake Jackson <admin@blueliquiddesigns.com.au>
2015-06-14 15:17:27 +1000

    Configure PHPUnit and write baseline tests for the mPDI (FPDI) classes
    - Update Readme to include details about unit testing
    
    - Add .gitignore file
    - Add `/tests/` directory and `.gitignore` files to .gitattributes's export-ignore functionality
    
    Refactor tests to more standardised format
    
    - Add PHPUnit to composer
    
    - Update readme to include instructions for installing composer packages and running unit tests through said package
    - Use composer's autoloader to include mPDF during unit tests
    - Update gitignore so the vendor directory is excluded

---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Setup our unit testing functionality
+ *
+ * @package    mPDF
+ * @author     Blue Liquid Designs <admin@blueliquiddesigns.com.au>
+ * @copyright  2015 Blue Liquid Designs
+ * @license    GPLv2
+ * @since      6.1.0
+ */
+
+/* Load the autoloader */
+require_once('../vendor/autoload.php');
+
+/* Create a new instance of the mPDF class */
+ /* We do this here to force the autoloader to include the actual file and its constants */
+ /* It means tests will have access to all of mPDF's constants without first creating a new instance (and everything is loaded) */
+new mPDF();
