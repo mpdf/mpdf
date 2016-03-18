356ed6a56fe711ed4b4279d0a3aa5c06bd5527a3
Matej Humpal <finwe@finwe.info>
2016-03-18 16:45:11 +0100

    includes/out.php code cleanup

--- includes/out.php
+++ includes/out.php
@@ -3,26 +3,32 @@
 $path = '../tmp/';
 
 $tempfilename = $_REQUEST['filename'].'.pdf';
-if (strstr($tempfilename,'/') || strstr($tempfilename,'\\')) { die("Filename should not contain \ or / "); }
+
+if (strstr($tempfilename,'/') || strstr($tempfilename,'\\')) {
+	throw new MpdfException('Output filename can not not contain \ or /');
+}
+
 $name = $_REQUEST['opname'];
 $dest = $_REQUEST['dest'];
-	if ($tempfilename && file_exists($path.$tempfilename)) {
+
+	if ($tempfilename && file_exists($path . $tempfilename)) {
 		// mPDF 5.3.17
-		if ($dest=='I') {
-			if(PHP_SAPI!='cli') {
+		if ($dest === 'I') {
+			if (PHP_SAPI != 'cli') {
 				header('Content-Type: application/pdf');
-				header('Content-disposition: inline; filename="'.$name.'"');
+				header('Content-disposition: inline; filename="' . $name . '"');
 				header('Cache-Control: public, must-revalidate, max-age=0');
 				header('Pragma: public');
 				header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
-				header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+				header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 			}
-		}
+		} elseif ($dest === 'D') {
 
-		else if ($dest=='D') {
+			if (headers_sent()) {
+				throw new MpdfException('Some data has already been output to browser, can\'t send PDF file');
+			}
+
 			header('Content-Description: File Transfer');
-			if (headers_sent())
-				$this->Error('Some data has already been output to browser, can\'t send PDF file');
 			header('Content-Transfer-Encoding: binary');
 			header('Cache-Control: public, must-revalidate, max-age=0');
 			header('Pragma: public');
@@ -32,26 +38,30 @@ $dest = $_REQUEST['dest'];
 			header('Content-Type: application/octet-stream', false);
 			header('Content-Type: application/download', false);
 			header('Content-Type: application/pdf', false);
-			header('Content-disposition: attachment; filename="'.$name.'"');
+			header('Content-disposition: attachment; filename="' . $name . '"');
 		}
+
 		$filesize = filesize($path.$tempfilename);
-		if (!isset($_SERVER['HTTP_ACCEPT_ENCODING']) OR empty($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+		if (empty($_SERVER['HTTP_ACCEPT_ENCODING'])) {
 			// don't use length if server using compression
-			header('Content-Length: '.$filesize);
+			header('Content-Length: ' . $filesize);
 		}
-		$fd=fopen($path.$tempfilename,'rb');
+
+		$fd = fopen($path . $tempfilename, 'rb');
 		fpassthru($fd);
 		fclose($fd);
-		unlink($path.$tempfilename);
+		unlink($path . $tempfilename);
+
 		// ====================== DELETE OLD FILES - Housekeeping =========================================
 		// Clear any files in directory that are >24 hrs old
+
 		$interval = 86400;
 		if ($handle = opendir(dirname($path.'dummy'))) {
-		   while (false !== ($file = readdir($handle))) {
-			if (((filemtime($path.$file)+$interval) < time()) && ($file != "..") && ($file != ".") && substr($file, -3)=='pdf') {
-				unlink($path.$file);
+			while (false !== ($file = readdir($handle))) {
+				if (((filemtime($path.$file)+$interval) < time()) && ($file != "..") && ($file != ".") && substr($file, -3)=='pdf') {
+					unlink($path.$file);
+				}
 			}
-		   }
 		   closedir($handle);
 		}
 		exit;
