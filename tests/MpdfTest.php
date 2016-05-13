38cd17c48350f55326b13a46f044611ad00bacf3
Matej Humpal <finwe@finwe.info>
2016-05-13 16:45:48 +0200

    Removed config files. Moved configuration definition to classes and constructor parameter

--- tests/MpdfTest.php
+++ tests/MpdfTest.php
@@ -25,6 +25,27 @@ class MpdfTest extends \PHPUnit_Framework_TestCase
 		$this->assertStringStartsWith('%PDF-', $output);
 	}
 
+	public function testDefaultSettings()
+	{
+		$mpdf = new Mpdf('', 'A4', 0, '', 15, 15, 16, 16, 9, 9, 'P', array());
+
+		$this->assertSame('1.4', $mpdf->pdf_version);
+		$this->assertSame(2000, $mpdf->maxTTFFilesize);
+		$this->assertFalse($mpdf->autoPadding);
+	}
+
+	public function testOverwrittenSettings()
+	{
+		$mpdf = new Mpdf('', 'A4', 0, '', 15, 15, 16, 16, 9, 9, 'P', array(
+			'pdf_version' => '1.5',
+			'autoPadding' => true,
+			'nonexisting_key' => true
+		));
+
+		$this->assertSame('1.5', $mpdf->pdf_version);
+		$this->assertTrue($mpdf->autoPadding);
+	}
+
 	/**
 	 * @expectedException Mpdf\MpdfException
 	 * @expectedExceptionMessage The HTML code size is larger than pcre.backtrack_limit
