a6e4d590f188abc0a5fc60e6debaee4a014616b4
Matej Humpal <finwe@finwe.info>
2017-03-08 18:47:45 +0100

    Code alignment in functions.php

--- src/functions.php
+++ src/functions.php
@@ -159,15 +159,15 @@ function codeHex2utf_lo_callback($matches)
 /**
  * PDF documents use the internal date format: (D:YYYYMMDDHHmmSSOHH'mm'). The date format has these parts:
  *
- *   YYYY	The full four-digit year. (For example, 2004)
- *   MM	The month from 01 to 12.
- *   DD	The day from 01 to 31.
- *   HH	The hour from 00 to 23.
- *   mm	The minute from 00 to 59.
- *   SS	The seconds from 00 to 59.
- *   O	The relationship of local time to Universal Time (UT), as denoted by one of the characters +, -, or Z.
- *   HH	The absolute value of the offset from UT in hours specified as 00 to 23.
- *   mm	The absolute value of the offset from UT in minutes specified as 00 to 59.
+ *   YYYY The full four-digit year. (For example, 2004)
+ *   MM   The month from 01 to 12.
+ *   DD   The day from 01 to 31.
+ *   HH   The hour from 00 to 23.
+ *   mm   The minute from 00 to 59.
+ *   SS   The seconds from 00 to 59.
+ *   O    The relationship of local time to Universal Time (UT), as denoted by one of the characters +, -, or Z.
+ *   HH   The absolute value of the offset from UT in hours specified as 00 to 23.
+ *   mm   The absolute value of the offset from UT in minutes specified as 00 to 59.
  *
  * @return string
  */
@@ -187,7 +187,7 @@ if (!function_exists('code2utf')) {
 	 */
 	function code2utf($num, $lo = true)
 	{
-		//Returns the utf string corresponding to the unicode value
+		// Returns the utf string corresponding to the unicode value
 		if ($num < 128) {
 			if ($lo) {
 				return chr($num);
