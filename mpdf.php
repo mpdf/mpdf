e7310ce53b5c11db1f0b781fe0a69066608ac6e2
Matej Humpal <finwe@finwe.info>
2017-07-09 18:44:53 +0200

    Fix testing temp files for directories on temp cleanup (Closes #435)

--- mpdf.php
+++ mpdf.php
@@ -9457,7 +9457,7 @@ class mPDF
 		$interval = 3600;
 		if ($handle = @opendir(preg_replace('/\/$/', '', _MPDF_TEMP_PATH))) { // mPDF 5.7.3
 			while (false !== ($file = readdir($handle))) {
-				if (($file != "..") && ($file != ".") && !is_dir($file) && ((filemtime(_MPDF_TEMP_PATH . $file) + $interval) < time()) && (substr($file, 0, 1) !== '.') && ($file != 'dummy.txt')) { // mPDF 5.7.3
+				if (($file != "..") && ($file != ".") && !is_dir(_MPDF_TEMP_PATH . $file) && ((filemtime(_MPDF_TEMP_PATH . $file) + $interval) < time()) && (substr($file, 0, 1) !== '.') && ($file != 'dummy.txt')) { // mPDF 5.7.3
 					unlink(_MPDF_TEMP_PATH . $file);
 				}
 			}
