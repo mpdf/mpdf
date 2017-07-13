179a0b89b4268f36182443942f71bc389d9a27d9
Matej Humpal <finwe@finwe.info>
2017-07-13 10:15:43 +0200

    Add issue 372 test data

--- tests/Issues/Issue372Test.php
+++ tests/Issues/Issue372Test.php
@@ -13,9 +13,10 @@ class Issue372Test extends \Mpdf\BaseMpdfTest
 		$this->mpdf->SetSourceFile(__DIR__ . '/../data/pdfs/2-Page-PDF_1_4.pdf');
 		$tplId = $this->mpdf->ImportPage(1);
 		$this->mpdf->UseTemplate($tplId);
-		$this->mpdf->SetWatermarkImage(__DIR__ . '../img/bayeux2.jpg', 1, '', [160, 10]);
+		$this->mpdf->SetWatermarkImage(__DIR__ . '/../data/img/bayeux2.jpg', 1, '', [160, 10]);
 		$this->mpdf->showWatermarkImage = true;
-		$this->mpdf->Output('', 'S');
+		$output = $this->mpdf->Output('', 'S');
+		$this->assertStringStartsWith('%PDF-', $output);
 	}
 
 }
