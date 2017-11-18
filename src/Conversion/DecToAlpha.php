e7841b9423853da11b882101b3c9991cbc299a25
Mehdi Achour <machour@gmail.com>
2017-11-18 17:45:37 +0100

    Strict checking, improve code readability

--- src/Conversion/DecToAlpha.php
+++ src/Conversion/DecToAlpha.php
@@ -13,8 +13,9 @@ class DecToAlpha
 			return '?'; //supports 'only' up to 18278
 		}
 
-		$c1 = $c2 = $c3 = '';
+		$c2 = $c3 = '';
 
+		$c1 = 64 + $valor; // 1 letter (up to 26)
 		if ($valor > 702) { // 3 letters (up to 18278)
 			$c1 = 65 + floor(($valor - 703) / 676);
 			$c2 = 65 + floor((($valor - 703) % 676) / 26);
@@ -22,20 +23,18 @@ class DecToAlpha
 		} elseif ($valor > 26) { // 2 letters (up to 702)
 			$c1 = (64 + (int) (($valor - 1) / 26));
 			$c2 = (64 + ($valor % 26));
-			if ($c2 == 64) {
+			if ($c2 === 64) {
 				$c2 += 26;
 			}
-		} else { // 1 letter (up to 26)
-			$c1 = (64 + $valor);
 		}
 
 		$alpha = chr($c1);
 
-		if ($c2 != '') {
+		if ($c2 !== '') {
 			$alpha .= chr($c2);
 		}
 
-		if ($c3 != '') {
+		if ($c3 !== '') {
 			$alpha .= chr($c3);
 		}
 
