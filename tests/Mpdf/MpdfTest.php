b824b06be7164247907e095f0e454a022db0588c
Paweł Krzaczkowski <pawel@freshmind.pl>
2017-11-22 11:08:43 +0100

    AssociatedFiles  - allow to include AssociatedFiles from source instead of file path

--- tests/Mpdf/MpdfTest.php
+++ tests/Mpdf/MpdfTest.php
@@ -38,7 +38,7 @@ class MpdfTest extends \PHPUnit_Framework_TestCase
 		$this->mpdf->AdjustHTML(str_repeat('a', ini_get('pcre.backtrack_limit') + 1));
 	}
 
-	public function testPdfAssociatedFiles()
+	public function testPdfAssociatedFilesPath()
 	{
 		$this->mpdf->PDFA = true;
 		$this->mpdf->PDFAauto = true;
@@ -59,6 +59,28 @@ class MpdfTest extends \PHPUnit_Framework_TestCase
 		$this->assertRegExp('/\d+ 0 obj\n<<\/Type \/EmbeddedFile\n\/Subtype \/text#2Fxml\n\/Length \d+\n\/Filter \/FlateDecode\n\/Params \<\<\/ModDate \(D:\d{14}[+|-|Z]\d{2}\'\d{2}\'\)/', $output);
 		$this->assertRegExp('/\/AF \d+ 0 R\n\/Names << \/EmbeddedFiles << \/Names \[\(public_filename\.xml\) \d+ 0 R\]/', $output);
 	}
+
+	 public function testPdfAssociatedFilesContent()
+	 {
+	 	 $this->mpdf->PDFA = true;
+	 	 $this->mpdf->PDFAauto = true;
+	 	 $this->mpdf->SetAssociatedFiles([[
+	 	 	 'name' => 'public_filename.xml',
+	 	 	 'mime' => 'text/xml',
+	 	 	 'description' => 'some description',
+	 	 	 'AFRelationship' => 'Alternative',
+	 	 	 'content' => '<?xml version="1.0" encoding="UTF-8"?><note><body>Hello World</body></note>'
+	 	 ]]);
+
+	 	 $this->mpdf->writeHtml('<html><body>hello world</body></html>');
+	 	 $output = $this->mpdf->Output(NULL, 'S');
+
+	 	 $this->assertStringStartsWith('%PDF-', $output);
+	 	 $this->assertRegExp('/\d+ 0 obj\n<<\/F \(public_filename\.xml\)\n\/Desc \(some description\)/', $output);
+	 	 $this->assertRegExp('/\/Type \/Filespec\n\/EF <<\n\/F \d+ 0 R\n>>\n\/AFRelationship \/Alternative/', $output);
+	 	 $this->assertRegExp('/\d+ 0 obj\n<<\/Type \/EmbeddedFile\n\/Subtype \/text#2Fxml\n\/Length \d+\n\/Filter \/FlateDecode\n\/Params \<\<\/ModDate \(D:\d{14}[+|-|Z]\d{2}\'\d{2}\'\)/', $output);
+	 	 $this->assertRegExp('/\/AF \d+ 0 R\n\/Names << \/EmbeddedFiles << \/Names \[\(public_filename\.xml\) \d+ 0 R\]/', $output);
+	 }
 
 	public function testPdfAdditionalXmpRdf()
 	{
