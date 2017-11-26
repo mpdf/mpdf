d7f1d0cd0d1827175506ae2cfffe883c9e391809
Mehdi Achour <machour@gmail.com>
2017-11-26 17:56:55 +0100

    refactor: migrate TR

--- src/Tag.php
+++ src/Tag.php
@@ -4,13 +4,9 @@ namespace Mpdf;
 
 use Mpdf\Color\ColorConverter;
 
-use Mpdf\Css\Border;
-
 use Mpdf\Image\ImageProcessor;
 
 use Mpdf\Language\LanguageToFontInterface;
-
-use Mpdf\Utils\UtfString;
 
 class Tag
 {
@@ -194,19 +190,6 @@ class Tag
 		if ($object = $this->getTagInstance($tag)) {
 			return $object->open($attr, $ahtml, $ihtml);
 		}
-
-		$align = [
-			'left' => 'L',
-			'center' => 'C',
-			'right' => 'R',
-			'top' => 'T',
-			'text-top' => 'TT',
-			'middle' => 'M',
-			'baseline' => 'BS',
-			'bottom' => 'B',
-			'text-bottom' => 'TB',
-			'justify' => 'J'
-		];
 
 		switch ($tag) {
 
@@ -730,67 +713,6 @@ class Tag
 
 			/* -- END COLUMNS -- */
 
-			/* -- TABLES -- */
-
-			case 'TR':
-				$this->mpdf->lastoptionaltag = $tag; // Save current HTML specified optional endtag
-				$this->cssManager->tbCSSlvl++;
-				$this->mpdf->row++;
-				$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['nr'] ++;
-				$this->mpdf->col = -1;
-				$properties = $this->cssManager->MergeCSS('TABLE', $tag, $attr);
-
-				if (!$this->mpdf->simpleTables && (!isset($this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['borders_separate'])
-						|| !$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['borders_separate'])) {
-					if (isset($properties['BORDER-LEFT']) && $properties['BORDER-LEFT']) {
-						$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['trborder-left'][$this->mpdf->row] = $properties['BORDER-LEFT'];
-					}
-					if (isset($properties['BORDER-RIGHT']) && $properties['BORDER-RIGHT']) {
-						$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['trborder-right'][$this->mpdf->row] = $properties['BORDER-RIGHT'];
-					}
-					if (isset($properties['BORDER-TOP']) && $properties['BORDER-TOP']) {
-						$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['trborder-top'][$this->mpdf->row] = $properties['BORDER-TOP'];
-					}
-					if (isset($properties['BORDER-BOTTOM']) && $properties['BORDER-BOTTOM']) {
-						$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['trborder-bottom'][$this->mpdf->row] = $properties['BORDER-BOTTOM'];
-					}
-				}
-
-				if (isset($properties['BACKGROUND-COLOR'])) {
-					$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['bgcolor'][$this->mpdf->row] = $properties['BACKGROUND-COLOR'];
-				} elseif (isset($attr['BGCOLOR'])) {
-					$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['bgcolor'][$this->mpdf->row] = $attr['BGCOLOR'];
-				}
-
-				/* -- BACKGROUNDS -- */
-				if (isset($properties['BACKGROUND-GRADIENT']) && !$this->mpdf->kwt && !$this->mpdf->ColActive) {
-					$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['trgradients'][$this->mpdf->row] = $properties['BACKGROUND-GRADIENT'];
-				}
-
-				if (isset($properties['BACKGROUND-IMAGE']) && $properties['BACKGROUND-IMAGE'] && !$this->mpdf->kwt && !$this->mpdf->ColActive) {
-					$ret = $this->mpdf->SetBackground($properties, $currblk['inner_width']);
-					if ($ret) {
-						$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['trbackground-images'][$this->mpdf->row] = $ret;
-					}
-				}
-				/* -- END BACKGROUNDS -- */
-
-				if (isset($properties['TEXT-ROTATE'])) {
-					$this->mpdf->trow_text_rotate = $properties['TEXT-ROTATE'];
-				}
-				if (isset($attr['TEXT-ROTATE'])) {
-					$this->mpdf->trow_text_rotate = $attr['TEXT-ROTATE'];
-				}
-
-				if ($this->mpdf->tablethead) {
-					$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['is_thead'][$this->mpdf->row] = true;
-				}
-				if ($this->mpdf->tabletfoot) {
-					$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['is_tfoot'][$this->mpdf->row] = true;
-				}
-				$properties = [];
-				break;
-			/* -- END TABLES -- */
 		}//end of switch
 	}
 
@@ -799,38 +721,5 @@ class Tag
 		if ($object = $this->getTagInstance($tag)) {
 			return $object->close($ahtml, $ihtml);
 		}
-
-		/* -- TABLES -- */
-
-		if ($tag == 'TR' && $this->mpdf->tableLevel) {
-			// If Border set on TR - Update right border
-			if (isset($this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['trborder-left'][$this->mpdf->row])) {
-				$c = & $this->mpdf->cell[$this->mpdf->row][$this->mpdf->col];
-				if ($c) {
-					if ($this->mpdf->packTableData) {
-						$cell = $this->mpdf->_unpackCellBorder($c['borderbin']);
-					} else {
-						$cell = $c;
-					}
-					$cell['border_details']['R'] = $this->mpdf->border_details(
-						$this->mpdf->table[$this->mpdf->tableLevel][$this->mpdf->tbctr[$this->mpdf->tableLevel]]['trborder-right'][$this->mpdf->row]
-					);
-					$this->mpdf->setBorder($cell['border'], Border::RIGHT, $cell['border_details']['R']['s']);
-					if ($this->mpdf->packTableData) {
-						$c['borderbin'] = $this->mpdf->_packCellBorder($cell);
-						unset($c['border']);
-						unset($c['border_details']);
-					} else {
-						$c = $cell;
-					}
-				}
-			}
-			$this->mpdf->lastoptionaltag = '';
-			unset($this->cssManager->tablecascadeCSS[$this->cssManager->tbCSSlvl]);
-			$this->cssManager->tbCSSlvl--;
-			$this->mpdf->trow_text_rotate = '';
-			$this->mpdf->tabletheadjustfinished = false;
-		}
-
 	}
 }
