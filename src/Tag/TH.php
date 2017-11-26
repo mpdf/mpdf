4f7dc103c88978e2ace274e1d20f644cb70ffa00
Mehdi Achour <machour@gmail.com>
2017-11-26 17:09:49 +0100

    refactor: Migrate TD and TH

---
+++ src/Tag/TH.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mpdf\Tag;
+
+
+class TH extends TD
+{
+
+	public function close(&$ahtml, &$ihtml)
+	{
+		$this->mpdf->SetStyle('B', false);
+		parent::close($ahtml, $ihtml);
+	}
+}
