5ceda384374d445e46e4ae4449cb22652a98ddd7
Mehdi Achour <machour@gmail.com>
2017-11-26 15:51:23 +0100

    refactor: migrate INDEXINSERT

--- src/Tag.php
+++ src/Tag.php
@@ -723,34 +723,6 @@ class Tag
 
 				break;
 
-
-
-			case 'INDEXINSERT':
-				if (isset($attr['COLLATION'])) {
-					$indexCollationLocale = $attr['COLLATION'];
-				} else {
-					$indexCollationLocale = '';
-				}
-				if (isset($attr['COLLATION-GROUP'])) {
-					$indexCollationGroup = $attr['COLLATION-GROUP'];
-				} else {
-					$indexCollationGroup = '';
-				}
-				if (isset($attr['USEDIVLETTERS']) && (strtoupper($attr['USEDIVLETTERS']) == 'OFF'
-						|| $attr['USEDIVLETTERS'] == -1
-						|| $attr['USEDIVLETTERS'] === '0')) {
-					$usedivletters = 0;
-				} else {
-					$usedivletters = 1;
-				}
-				if (isset($attr['LINKS']) && (strtoupper($attr['LINKS']) == 'ON' || $attr['LINKS'] == 1)) {
-					$links = true;
-				} else {
-					$links = false;
-				}
-				$this->mpdf->InsertIndex($usedivletters, $links, $indexCollationLocale, $indexCollationGroup);
-
-				break;
 			/* -- END INDEX -- */
 
 			/* -- WATERMARK -- */
