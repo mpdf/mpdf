c1391ca8f712aa894c704322043d0933e9d43253
Mehdi Achour <machour@gmail.com>
2017-11-26 15:53:58 +0100

    refactor: migrate WATERMARKIMAGE

--- src/Tag.php
+++ src/Tag.php
@@ -725,39 +725,6 @@ class Tag
 
 			/* -- END INDEX -- */
 
-			/* -- WATERMARK -- */
-			
-
-			case 'WATERMARKIMAGE':
-				if (isset($attr['SRC'])) {
-					$src = $attr['SRC'];
-				} else {
-					$src = '';
-				}
-				if (isset($attr['ALPHA']) && $attr['ALPHA'] > 0) {
-					$alpha = $attr['ALPHA'];
-				} else {
-					$alpha = -1;
-				}
-				if (isset($attr['SIZE']) && $attr['SIZE']) {
-					$size = $attr['SIZE'];
-					if (strpos($size, ',')) {
-						$size = explode(',', $size);
-					}
-				} else {
-					$size = 'D';
-				}
-				if (isset($attr['POSITION']) && $attr['POSITION']) {  // mPDF 5.7.2
-					$pos = $attr['POSITION'];
-					if (strpos($pos, ',')) {
-						$pos = explode(',', $pos);
-					}
-				} else {
-					$pos = 'P';
-				}
-				$this->mpdf->SetWatermarkImage($src, $alpha, $size, $pos);
-				break;
-			/* -- END WATERMARK -- */
 
 			/* -- BOOKMARKS -- */
 			case 'BOOKMARK':
