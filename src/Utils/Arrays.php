eada091506bbe203cb8d3247c840849abe62bbc8
Thiago Guimarães <webmailcontatos@gmail.com>
2018-01-12 09:37:32 -0200

    Fix bug  #516

--- src/Utils/Arrays.php
+++ src/Utils/Arrays.php
@@ -5,17 +5,29 @@ namespace Mpdf\Utils;
 class Arrays
 {
 
-	public static function get($array, $key, $default = null)
-	{
-		if (is_array($array) && array_key_exists($key, $array)) {
-			return $array[$key];
-		}
+    public static function get($array, $key, $default = null)
+    {
+        if (is_array($array) && array_key_exists($key, $array)) {
+            return $array[$key];
+        }
 
-		if (func_num_args() < 3) {
-			throw new \InvalidArgumentException(sprintf('Array does not contain key "%s"', $key));
-		}
+        if (func_num_args() < 3) {
+            throw new \InvalidArgumentException(sprintf('Array does not contain key "%s"', $key));
+        }
 
-		return $default;
-	}
+        return $default;
+    }
+
+    /**
+     * Find value in array and return it
+     * @param array $array   Array with values
+     * @param mixed $key     Key
+     * @param mixed $default Value default returned if not found key
+     * @return mixed
+     */
+    public static function findValue($array, $key, $default = null)
+    {
+        return empty($array[$key]) === true ? $default : $array[$key];
+    }
 
 }
