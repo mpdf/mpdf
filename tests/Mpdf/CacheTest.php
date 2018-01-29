c06d29e02e6dac9ba5720c284a6c96360619f9c8
Vladimir Pyatnitskiy <work@vpyatnitskiy.com>
2018-01-29 19:39:13 +0300

    Fix handling permissions on temporary directories
    Issue:
    
    `Mpdf\Cache` used `mkdir()` to create missing temporary directories recursively,
    and to make them world-writable (file mode 0777).
    
    However, `mkdir($path, 0777, true)` takes umask into account when assigning file mode
    (permissions) after creating a directory.
    
    It's not unusual to have umask 0002 or 0022, which in this case causes the temp dirs
    not to be world-writable. This state ultimately leads to failures when a temp dir is
    accessed by a user different than the one who created the directory (i.e., web server
    vs cli worker).
    
    Fix:
    
    Use `chmod()`, which doesn't take umask into account, after `mkdir()`.
    
    Since `chmod()` does not support recursive application, it was necessary to implement
    recursion manually, where the `Mpdf\Cache::createBasePath()` function recursively
    creates parent directories before attempting to create, and assign permissions to,
    the target directory.

---
+++ tests/Mpdf/CacheTest.php
@@ -0,0 +1,102 @@
+<?php
+
+
+namespace Mpdf;
+
+class CacheTest extends \PHPUnit_Framework_TestCase
+{
+	protected $basePath;
+	protected $oldTmpMode;
+
+	public function __construct($name = null, array $data = [], $dataName = '')
+	{
+		parent::__construct($name, $data, $dataName);
+
+		$this->basePath = __DIR__ . "/../../";
+	}
+
+	protected function path($relativeToRoot)
+	{
+		return $this->basePath . $relativeToRoot;
+	}
+
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$dir = $this->path("tmp");
+		$this->oldTmpMode = fileperms($dir);
+		chmod($dir, 0777);
+	}
+
+	protected function tearDown()
+	{
+		chmod($this->path("tmp"), $this->oldTmpMode);
+
+		parent::tearDown();
+	}
+
+	public function testCacheCreatesNonexistentDirectory()
+	{
+		$dir = $this->path("tmp/test1");
+
+		try {
+			new Cache($dir);
+
+			$this->assertDirectoryExists($dir);
+
+			$this->assertTrue(file_exists($dir));
+		} finally {
+			@rmdir($dir);
+		}
+	}
+
+	public function testCreatedDirectoryIsWorldWritable()
+	{
+		$dir = $this->path("tmp/test2");
+
+		try {
+			new Cache($dir);
+
+			$this->assertEquals(0777, fileperms($dir) & 0777);
+		} finally {
+			@rmdir($dir);
+		}
+	}
+
+	public function testCacheCreatesDirectoriesRecursively()
+	{
+		$dir = $this->path("tmp/test3/subdir/subdir2");
+
+		try {
+			new Cache($dir);
+
+			$this->assertDirectoryExists($dir);
+		} finally {
+			@rmdir($dir);
+			@rmdir($this->path("tmp/test3/subdir"));
+			@rmdir($this->path("tmp/test3"));
+		}
+	}
+
+	public function testRecursivelyCreatedDirectoriesAreWorldWritable()
+	{
+		$dir = $this->path("tmp/test4/subdir/subdir2");
+
+		try {
+			new Cache($dir);
+
+			foreach (array(
+						 "tmp/test4/subdir/subdir2",
+						 "tmp/test4/subdir",
+						 "tmp/test4",
+					 ) as $subdir) {
+				$this->assertEquals(0777, fileperms($this->path($subdir)) & 0777);
+			}
+		} finally {
+			@rmdir($dir);
+			@rmdir($this->path("tmp/test4/subdir"));
+			@rmdir($this->path("tmp/test4"));
+		}
+	}
+}
