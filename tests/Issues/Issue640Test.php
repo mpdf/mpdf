2115adcf4dfd9d8a2eefba0afdbacb2ff4665325
Jake Jackson <jake@gravitypdf.com>
2018-02-09 14:01:09 +1100

    Fix 0.1mm whitespace when using footer with zero margins
    ```
    $mpdf = new \Mpdf\Mpdf();
    $mpdf->WriteHTML('
    <style>
    @page {
    	footer: html_myFooter1;
    	margin-footer: 0;
    }
    
    height: 25mm;
    background:red;
    }
    </style>
    
    <htmlpagefooter name="myFooter1">
    	<div id="bottom">test</div>
    </htmlpagefooter>
    ');
    ```
    
    Before the PR, the footer would be set to 0.1mm from the bottom of the page to prevent a division by zero. However, this would leave a noticeable small white line. This PR will push the footer down below the page edge by 0.01 instead.

---
+++ tests/Issues/Issue640Test.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Issues;
+
+class Issue640Test extends \Mpdf\BaseMpdfTest
+{
+
+	public function testBottomMargin()
+	{
+		$this->mpdf->WriteHTML('	
+		<style>
+			@page {
+				footer: html_myFooter1;
+				margin-footer: 0;
+			}
+			
+			#bottom {
+			height: 25mm;
+			background:red;
+			}
+		</style>
+		
+		<htmlpagefooter name="myFooter1">
+			<div id="bottom">test</div>
+		</htmlpagefooter>');
+
+		$this->mpdf->Close();
+
+		$this->assertRegexp('/1.0000 0.0000 0.0000 1.0000 0.0000 -725.6979 cm/', $this->mpdf->pages[1]);
+	}
+
+}
