76d60098125c7644aabcf32bd1923bd6524381cc
Jake Jackson <jake@gravitypdf.com>
2018-02-12 13:38:49 +1100

    Ensure self-closing tags with no attributes are processed when no space between tag and slash
    This issue is most prominent with the <pagebreak/> tag, but does effect all other control tags when no attributes are used (i.e <pagebreak orientation="L"/> isn't effected)

---
+++ tests/Issues/Issue645Test.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Issues;
+
+class Issue645Test extends \Mpdf\BaseMpdfTest
+{
+
+	public function testFixSelfClosingTag()
+	{
+		$this->mpdf->WriteHTML('
+		Page 1
+		
+		<pagebreak />
+		
+		Page 2
+		
+		<pagebreak/>
+		
+		Page 3
+		
+		<pagebreak>
+		
+		Page 4
+		');
+
+		$this->mpdf->Close();
+
+		$this->assertSame(4, count($this->mpdf->pages));
+	}
+
+}
