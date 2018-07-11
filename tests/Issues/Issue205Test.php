5bf3643f572d1236bc16477beae768d52e00e9ca
Jasmin Besic <jasmin.besa@gmail.com>
2018-07-11 22:51:27 +0200

    #205 Convert spaces to tabs

--- tests/Issues/Issue205Test.php
+++ tests/Issues/Issue205Test.php
@@ -7,24 +7,24 @@ use Mpdf\Mpdf;
 class Issue205Test extends \Mpdf\BaseMpdfTest
 {
 
-    public function testBackgroundImagePatternSteps()
-    {
-        $style  = 'background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAQAAAC0NkA6AAAALklEQVR42u3NMREAAAgEIL//mVkzOLhBAdJT7yKRSCQSiUQikUgkEolEIpFIbhbYFE2LUERN2gAAAABJRU5ErkJggg==);';
-        $style .= 'background-repeat: no-repeat;';
-        $style .= 'background-size: 350px 350px';
-        $style .= 'height: 1500px;';
+	public function testBackgroundImagePatternSteps()
+	{
+		$style  = 'background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAQAAAC0NkA6AAAALklEQVR42u3NMREAAAgEIL//mVkzOLhBAdJT7yKRSCQSiUQikUgkEolEIpFIbhbYFE2LUERN2gAAAABJRU5ErkJggg==);';
+		$style .= 'background-repeat: no-repeat;';
+		$style .= 'background-size: 350px 350px';
+		$style .= 'height: 1500px;';
 
-        $html = '<div style ="' . $style . '"></div>';
+		$html = '<div style ="' . $style . '"></div>';
 
-        $this->mpdf->WriteHtml($html);
+		$this->mpdf->WriteHtml($html);
 
-        $output = $this->mpdf->Output('', 'S');
+		$output = $this->mpdf->Output('', 'S');
 
-        $xStepPosition = strpos($output, 'XStep 512');
-        $this->assertTrue($xStepPosition > 0);
+		$xStepPosition = strpos($output, 'XStep 512');
+		$this->assertTrue($xStepPosition > 0);
 
-        $yStepPosition = strpos($output, 'XStep 512');
-        $this->assertTrue($yStepPosition > 0);
-    }
+		$yStepPosition = strpos($output, 'XStep 512');
+		$this->assertTrue($yStepPosition > 0);
+	}
 
 }
