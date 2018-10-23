b04c9ae3d53a216780898b7e0fd9ebfe9a4161c4
Enrique Lacoma <elacomap@everis.com>
2018-10-23 17:33:40 +0200

    Remove timezone override, to avoid conflict when mpdf is used with other libraries.

--- mpdf.php
+++ mpdf.php
@@ -90,12 +90,6 @@ $errorlevel = error_reporting($errorlevel & ~E_NOTICE);
 
 //error_reporting(E_ALL);
 
-if (function_exists("date_default_timezone_set")) {
-	if (ini_get("date.timezone") == "") {
-		date_default_timezone_set("Europe/London");
-	}
-}
-
 if (!function_exists('mb_strlen')) {
 	throw new MpdfException('mPDF requires mb_string functions. Ensure that mb_string extension is loaded.');
 }
