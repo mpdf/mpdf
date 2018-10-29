b30958436a2fc8916b9fb31bdd7831aa294271ff
Jake Jackson <jake@gravitypdf.com>
2018-10-29 15:35:29 +1100

    Allow the non-active form radio field color to be set with CSS

---
+++ tests/Issues/Issue897.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Issues;
+
+use Mpdf\Output\Destination;
+
+class Issue897Test extends \Mpdf\BaseMpdfTest
+{
+
+	public function testSetHeader()
+	{
+		$this->mpdf->WriteHTML('
+		<style>
+			.radio {
+				color: green;
+			}
+		</style>
+		
+    	<input class="radio" type="radio" checked="checked"/>
+    	
+    	<pagebreak/>
+    	
+    	<input type="radio" checked="checked"/>
+		');
+
+		$this->mpdf->Close();
+
+		$this->assertRegexp('/0.000 0.502 0.000 rg/', $this->mpdf->pages[1]);
+		$this->assertRegexp('/0.000 0.502 0.000 RG/', $this->mpdf->pages[1]);
+
+		$this->assertNotRegexp('/0.000 0.502 0.000 rg/', $this->mpdf->pages[2]);
+		$this->assertNotRegexp('/0.000 0.502 0.000 RG/', $this->mpdf->pages[2]);
+	}
+
+}
