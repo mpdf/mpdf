dfa97cd77d5d3cf2699a212ec77ab59f4f776ba9
Gabriel Caruso <carusogabriel34@gmail.com>
2018-11-19 01:57:56 -0200

    Use dedicated PHPUnit assertions

--- tests/Mpdf/Barcode/Rm4SccTest.php
+++ tests/Mpdf/Barcode/Rm4SccTest.php
@@ -19,7 +19,7 @@ class Rm4SccTest extends \PHPUnit_Framework_TestCase
 
 		$this->assertInternalType('array', $array);
 		$this->assertArrayHasKey('maxh', $array);
-		$this->assertTrue($array['maxh'] > 0);
+		$this->assertGreaterThan(0, $array['maxh']);
 	}
 
 }
