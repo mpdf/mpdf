9e18f7cc6cf744f7b238f211a002982c80f3a8e4
Matthias Kühne <matthias.kuehne@ellerhold.de>
2018-12-12 13:01:04 +0100

    More tabs, less spaces!

--- src/FpdiPdfParser.php
+++ src/FpdiPdfParser.php
@@ -4,11 +4,11 @@ namespace Mpdf;
 
 class FpdiPdfParser extends \fpdi_pdf_parser
 {
-    /**
-     * @return array
-     */
-    public function getPages()
-    {
-        return $this->_pages;
-    }
+	/**
+	 * @return array
+	 */
+	public function getPages()
+	{
+		return $this->_pages;
+	}
 }
--- tests/Issues/Issue900Test.php
+++ tests/Issues/Issue900Test.php
@@ -8,36 +8,36 @@ class Issue900Test extends \Mpdf\BaseMpdfTest
 {
 	public function testMergePdfWithLinks()
 	{
-	    $tempfile = tempnam($this->mpdf->tempDir, 'i900-raw');
+		$tempfile = tempnam($this->mpdf->tempDir, 'i900-raw');
 
-	    $this->mpdf->WriteHTML('<!DOCTYPE html><html><body><a href="https://example.org">My Link</a></body></html>');
-	    $this->mpdf->Output($tempfile, DESTINATION::FILE);
+		$this->mpdf->WriteHTML('<!DOCTYPE html><html><body><a href="https://example.org">My Link</a></body></html>');
+		$this->mpdf->Output($tempfile, DESTINATION::FILE);
 
-	    // Reset MPDF
-	    $this->setUp();
+		// Reset MPDF
+		$this->setUp();
 
 		$this->mpdf->SetImportUse();
 		$pageCount = $this->mpdf->SetSourceFile($tempfile);
 
-        for ($page = 1; $page <= $pageCount; $page++) {
-            $this->mpdf->AddPage();
-            $template = $this->mpdf->ImportPage($page);
-            $this->mpdf->UseTemplate($template);
-        }
+		for ($page = 1; $page <= $pageCount; $page++) {
+			$this->mpdf->AddPage();
+			$template = $this->mpdf->ImportPage($page);
+			$this->mpdf->UseTemplate($template);
+		}
 
-        unlink($tempfile);
+		unlink($tempfile);
 
-        $importedFile = $this->mpdf->Output('', Destination::STRING_RETURN);
+		$importedFile = $this->mpdf->Output('', Destination::STRING_RETURN);
 
-        self::assertContains(
-            'https://example.org',
-            $importedFile,
-            'Link-Target should be in the imported PDF.'
-        );
-        self::assertContains(
-            '/Annot /Subtype /Link',
-            $importedFile,
-            'The Link should be in the imported PDF.'
-        );
+		self::assertContains(
+			'https://example.org',
+			$importedFile,
+			'Link-Target should be in the imported PDF.'
+		);
+		self::assertContains(
+			'/Annot /Subtype /Link',
+			$importedFile,
+			'The Link should be in the imported PDF.'
+		);
 	}
 }
