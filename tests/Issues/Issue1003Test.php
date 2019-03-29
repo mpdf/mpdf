9e834dd3ef286e9c2cd3eeb8a99d853f4d8bcaab
Jake Jackson <jake@gravitypdf.com>
2019-03-29 12:01:55 +1100

    Fix PHP notice due to `auto` on padding_left / padding_right parameter
    Resolve #1003

---
+++ tests/Issues/Issue1003Test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Issues;
+
+class Issue1003Test extends \PHPUnit_Framework_TestCase
+{
+
+	public function testNoNoticeWithAutoPaddingRight()
+	{
+		$mpdf = new \Mpdf\Mpdf();
+		$mpdf->WriteHTML('
+	    <style>
+            .list { width: 501px; margin: 42px auto 0; padding-left: 41px; }
+        </style>
+        
+        <ul class="list">
+            <li>some text</li>
+        </ul>
+		');
+
+		$output = $mpdf->output('', 'S');
+		$this->assertStringStartsWith('%PDF-', $output);
+	}
+
+}
