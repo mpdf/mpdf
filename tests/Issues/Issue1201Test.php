144630261fed1c03543a623c41c75e7f49f1a4ee
minkyawnyunt <mgminkyawnyunt@gmail.com>
2020-04-12 11:00:05 +0630

    Add unit test/sample for page number myanmar language

---
+++ tests/Issues/Issue1201Test.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Issues;
+use Mpdf\Output\Destination;
+
+class Issue1201Test extends \PHPUnit_Framework_TestCase
+{
+
+	public function testPageNumberMyanmarLanguage()
+	{
+		$mpdf = new \Mpdf\Mpdf([
+            'default_font' => 'Tharlon',
+            'defaultPageNumStyle' => 'myanmar'
+        ]);
+        $mpdf->setFooter('{PAGENO}');
+        $mpdf->AddPage();
+        $mpdf->AddPage();
+        $mpdf->AddPage();
+
+        $output = $mpdf->output('', 'S');
+		$this->assertStringStartsWith('%PDF-', $output);
+	}
+
+}
