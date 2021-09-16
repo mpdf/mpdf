2140703f247d912351ebe6e034adde5644be804a
sandreas <2050604+sandreas@users.noreply.github.com>
2021-09-16 13:01:00 +0200

    fix #1504 - overcome fread limit in buffered streams (e.g. phar)

--- src/TTFontFile.php
+++ src/TTFontFile.php
@@ -511,15 +511,27 @@ class TTFontFile
 		return $this->splice($stream, $offset, $up);
 	}
 
-	function get_chunk($pos, $length)
-	{
-		fseek($this->fh, $pos);
-		if ($length < 1) {
-			return '';
-		}
+    function get_chunk($pos, $length)
+    {
+        fseek($this->fh, $pos);
+        if ($length < 1) {
+            return '';
+        }
 
-		return (fread($this->fh, $length));
-	}
+        $data = (fread($this->fh, $length));
+
+        // fix for #1504
+        // if fread is used to read from a compressed / buffered stream (e.g. phar://...)
+        // the $length parameter will be ignored - fread is limited in size (usually 8192 bytes)
+        // to fix this, the data length must be checked after reading. If the read was incomplete,
+        // try to read the rest of the data
+        $dataLen = strlen($data);
+        while($dataLen < $length && !feof($this->fh)) {
+            $data .= fread($this->fh, $length - $dataLen);
+            $dataLen = strlen($data);
+        }
+        return $data;
+    }
 
 	function get_table($tag)
 	{
