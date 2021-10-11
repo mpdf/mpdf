bc14d3b650dc3e6441fb89d8d482b6b4054e53d8
Matej Humpal <finwe@finwe.info>
2021-10-11 16:26:11 +0200

    Fix indentation in Issue1526Test

--- tests/Issues/Issue1526Test.php
+++ tests/Issues/Issue1526Test.php
@@ -9,17 +9,18 @@ use PHPUnit\Framework\TestCase;
 class Issue1526Test extends TestCase
 {
 
-    public function testDoNotThrowUndefinedOffsetError()
-    {
-        $pdf = new Mpdf();
-        $pdf->AddPage();
-        $pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
-        $pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
-        $pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
-        $pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
-        $pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
-        $pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
-        $result = $pdf->Output(null, Destination::STRING_RETURN);
-        $this->assertNotNull($result);
-    }
+	public function testDoNotThrowUndefinedOffsetError()
+	{
+		$pdf = new Mpdf();
+		$pdf->AddPage();
+		$pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
+		$pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
+		$pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
+		$pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
+		$pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
+		$pdf->MultiCell(4, 3, 'ISO 17 025 - Ensaios', 0, 'L');
+		$result = $pdf->Output(null, Destination::STRING_RETURN);
+		$this->assertNotNull($result);
+	}
+
 }
