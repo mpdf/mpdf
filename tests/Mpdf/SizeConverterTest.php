5c79c1ab500e5512400ef637c5ae04804f8be9ed
Matej Humpal <finwe@finwe.info>
2022-03-24 22:56:41 +0100

    Use assertEqualsWithDelta() in sizeconverter test

--- tests/Mpdf/SizeConverterTest.php
+++ tests/Mpdf/SizeConverterTest.php
@@ -38,7 +38,7 @@ class SizeConverterTest extends \Yoast\PHPUnitPolyfills\TestCases\TestCase
 	 */
 	public function testConvert($size, $maxsize, $fontsize, $usefontsize, $converted)
 	{
-		$this->assertSame($converted, $this->converter->convert($size, $maxsize, $fontsize, $usefontsize));
+		$this->assertEqualsWithDelta($converted, $this->converter->convert($size, $maxsize, $fontsize, $usefontsize), 0.0000001);
 	}
 
 	public function sizesProvider()
