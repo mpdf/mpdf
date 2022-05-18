c1b6de69de82b2fceda058a29e9839c19d61b087
Michael-Labriola <mike@mikelab.com>
2022-05-18 09:31:58 -0400

    Update Mpdf.php
    I periodically get an "Undefined array key -1" error when $bk = 0 and $bk-1 = -1.  I don't know if this is the best way to fix this, but changing the condition so $bk-1 is not referenced when $bk = 0 makes the error go away and the resulting PDF is still correct.

--- src/Mpdf.php
+++ src/Mpdf.php
@@ -24547,7 +24547,7 @@ class Mpdf implements \Psr\Log\LoggerAwareInterface
 				$th = ($sum_h * $i / $this->NbCol);
 				foreach ($breaks as $bk => $val) {
 					if ($val > $th) {
-						if (($val - $th) < ($th - $breaks[$bk - 1])) {
+						if (!$bk || ($val - $th) < ($th - $breaks[$bk - 1])) {
 							$cbr[$i - 1] = $val;
 						} else {
 							$cbr[$i - 1] = $breaks[$bk - 1];
