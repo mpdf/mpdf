f4d2afec3c192e928530d412840d9c48d81031ce
Matej Humpal <finwe@finwe.info>
2023-05-03 21:46:48 +0200

    Bump version constraint

--- src/Mpdf.php
+++ src/Mpdf.php
@@ -32,7 +32,7 @@ class Mpdf implements \Psr\Log\LoggerAwareInterface
 	use FpdiTrait;
 	use MpdfPsrLogAwareTrait;
 
-	const VERSION = '8.1.6';
+	const VERSION = '8.1.7';
 
 	const SCALE = 72 / 25.4;
 
