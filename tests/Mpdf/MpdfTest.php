60981b4501ed4c7d176617050bf21957d0de2dce
Carl-Philip Hänsch <carli@launix.de>
2024-04-02 20:22:45 +0200

    Fix CS

--- tests/Mpdf/MpdfTest.php
+++ tests/Mpdf/MpdfTest.php
@@ -45,8 +45,7 @@ class MpdfTest extends \Yoast\PHPUnitPolyfills\TestCases\TestCase
 		// test case: spill items that take about a bit more than half a page, no page-break-avoid would fit them on two pages, with page-break it will be three
 		$html = '';
 		$itemsPerTwothirdsPage = 28;
-		for ($i = 0; $i < 3*$itemsPerTwothirdsPage; $i++)
-		{
+		for ($i = 0; $i < 3*$itemsPerTwothirdsPage; $i++) {
 			if ($i % $itemsPerTwothirdsPage == 0) {
 				$html .= '<tr style=""><td>groupheader</td></tr>';
 			} else {
