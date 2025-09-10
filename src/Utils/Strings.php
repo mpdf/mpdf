1bf27d9a22cc3e5bdc18473f0414fd30b3cf2bb9
Denny Septian Panggabean <xamidimura@gmail.com>
2025-09-10 17:41:16 +0700

    fix: style

--- src/Utils/Strings.php
+++ src/Utils/Strings.php
@@ -4,8 +4,8 @@ namespace Mpdf\Utils;
 
 class Strings
 {
-    public static function incrementString(string $str = '')
-    {
-        return PHP_VERSION_ID === 80500 ? str_increment($str) : ++$str;
-    }
+  public static function incrementString(string $str = '')
+  {
+    return PHP_VERSION_ID === 80500 ? str_increment($str) : ++$str;
+  }
 }
