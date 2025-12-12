cc184b6774e4b6a6ecbfd3886acca401159a99d8
Jake Jackson <jake@gravitypdf.com>
2025-12-12 15:31:25 +1100

    Enhance external link importer

--- tests/Mpdf/FpdiTest.php
+++ tests/Mpdf/FpdiTest.php
@@ -7,14 +7,18 @@ use Mpdf\Pdf\Protection;
 use Mpdf\Pdf\Protection\UniqidGenerator;
 use Mpdf\Writer\BaseWriter;
 use pdf_parser;
-use ReflectionClass;
 use setasign\Fpdi\PdfParser\PdfParser;
 use setasign\Fpdi\PdfParser\StreamReader;
+use setasign\Fpdi\PdfParser\Type\PdfArray;
+use setasign\Fpdi\PdfParser\Type\PdfBoolean;
 use setasign\Fpdi\PdfParser\Type\PdfDictionary;
+use setasign\Fpdi\PdfParser\Type\PdfName;
 use setasign\Fpdi\PdfParser\Type\PdfNull;
+use setasign\Fpdi\PdfParser\Type\PdfNumeric;
 use setasign\Fpdi\PdfParser\Type\PdfStream;
 use setasign\Fpdi\PdfParser\Type\PdfString;
 use setasign\Fpdi\PdfParser\Type\PdfType;
+use setasign\Fpdi\PdfReader\DataStructure\Rectangle;
 use setasign\Fpdi\PdfReader\PageBoundaries;
 use setasign\Fpdi\PdfReader\PdfReader;
 
@@ -135,7 +139,7 @@ class FpdiTest extends \Yoast\PHPUnitPolyfills\TestCases\TestCase
 		$pdf = new mPDF();
 		$writer = new BaseWriter($pdf, new Protection(new UniqidGenerator()));
 
-		$pdf->SetProtection(['copy','print'], '', 'password', 128);
+		$pdf->SetProtection(['copy', 'print'], '', 'password', 128);
 
 		$string = new PdfString();
 		$string->value = '\040\t\n\f\040';
@@ -384,7 +388,7 @@ class FpdiTest extends \Yoast\PHPUnitPolyfills\TestCases\TestCase
 			$contentStream = $page->getContentStream();
 
 			// check for the background
-			$backgroundName = ($pageNo & 1) === 1 ? 'TPL0' :  'TPL1';
+			$backgroundName = ($pageNo & 1) === 1 ? 'TPL0' : 'TPL1';
 			$this->assertNotFalse(strpos($contentStream, '/' . $backgroundName));
 			// and the imported page
 			$this->assertNotFalse(strpos($contentStream, '/TPL' . ($pageNo + 1)));
@@ -415,5 +419,486 @@ class FpdiTest extends \Yoast\PHPUnitPolyfills\TestCases\TestCase
 		$pageTwo = $pdfReader->getPage(2);
 		$contentStream = $pageTwo->getContentStream();
 		$this->assertSame(1, preg_match_all('~/TPL\d~', $contentStream));
+	}
+
+	/**
+	 * @copyright Copyright (c) Setasign GmbH & Co. KG (https://www.setasign.com)
+	 * @license http://opensource.org/licenses/mit-license The MIT License
+	 */
+	public function testExternalLinksAndStyles()
+	{
+		$mpdf = new Mpdf();
+		$mpdf->AddPage();
+		$mpdf->setSourceFile(__DIR__ . '/../data/pdfs/external-links/links.pdf');
+		$tplId = $mpdf->importPage(1);
+		$mpdf->useTemplate($tplId, [
+			'x' => 20,
+			'y' => 20,
+			'width' => 100,
+			'height' => 100
+		]);
+		$mpdf->Rect(20, 20, 100, 100);
+
+		$pdfString = $mpdf->OutputBinaryData();
+
+		$expectedLinks = [
+			[
+				'uri' => 'https://www.setasign.com/#1',
+				'rect' => [174.93, 761.11, 209.68, 766.3],
+				'color' => false,
+				'border' => [0, 0, 1, [3]],
+				'borderStyle' => [
+					'D' => [3],
+					'S' => 'D',
+					'Type' => 'Border',
+					'W' => 1
+				]
+			],
+			[
+				'uri' => 'https://www.setasign.com/#2',
+				'rect' => [83.26, 743.38, 302.55, 751.01],
+				'f' => 4,
+				'color' => [1, 0, 0],
+				'border' => false,
+				'borderStyle' => [
+					'S' => 'S',
+					'W' => 1,
+				]
+			],
+			[
+				'uri' => 'https://www.setasign.com/#4',
+				'rect' => [243.22, 705.37, 312.27, 709.72],
+				'f' => 4,
+				'color' => [1, 0, 0],
+				'border' => false,
+				'borderStyle' => [
+					'D' => [3],
+					'S' => 'D',
+					'W' => 0
+				],
+			],
+			[
+				'uri' => 'https://www.setasign.com/#5',
+				'rect' => [113.21, 696.08, 137.67, 700.43],
+				'f' => 4,
+				'color' => [0.376471, 0.74902, 0],
+				'borderStyle' => [
+					'W' => 1,
+					'S' => 'D',
+					'D' => [3]
+				]
+
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?some=(get paramert/with special signs',
+				'rect' => [168.4, 682.12, 215.38, 687.31],
+				'border' => [0, 0, 1, [3]],
+				'borderStyle' => [
+					'W' => 1,
+					'S' => 'D',
+					'D' => [3]
+				]
+			],
+			[
+				'uri' => 'https://www.setasign.com/#3',
+				'rect' => [83.74, 719.29, 312.5, 729.13],
+				'quadPoints' => [298.54, 723.94, 312.50, 723.94, 312.50, 729.13, 298.54, 729.13, 83.74, 719.29, 95.78, 719.29, 95.78, 724.48, 83.74, 724.48],
+				'color' => [0.25, 0.333328, 1],
+				'border' => [0, 0, 3],
+				'borderStyle' => [
+					'S' => 'S',
+					'W' => 3
+				]
+			]
+		];
+
+		$reader = new PdfReader(new PdfParser(StreamReader::createByString($pdfString)));
+		$this->compareExpectedLinks(1, $expectedLinks, $reader);
+	}
+
+	/**
+	 * @copyright Copyright (c) Setasign GmbH & Co. KG (https://www.setasign.com)
+	 * @license http://opensource.org/licenses/mit-license The MIT License
+	 */
+	public function testExternalLinksOnRotatedPages()
+	{
+		$mpdf = new Mpdf();
+		$pageCount = $mpdf->setSourceFile(__DIR__ . '/../data/pdfs/external-links/rotated-pages.pdf');
+		for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
+			$mpdf->AddPage();
+			$tplId = $mpdf->importPage($pageNo);
+			$mpdf->useTemplate($tplId, ['adjustPageSize' => true]);
+		}
+
+		$pdfString = $mpdf->OutputBinaryData();
+
+		$expectedPageLinks = [
+			1 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [414.56, 151.34, 447.88, 297.64],
+				]
+			],
+			2 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [135.77, 394.01, 297.64, 427.33],
+				]
+			],
+			3 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [394.01, 297.64, 427.33, 459.51],
+				]
+			],
+			4 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [297.64, 414.56, 459.51, 447.88],
+				]
+			],
+			5 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [414.56, 135.77, 447.88, 297.64],
+				]
+			],
+			6 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [394.01, 297.64, 427.33, 453.26],
+				]
+			],
+			7 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [126.45, 394.01, 297.64, 427.33],
+				]
+			],
+			8 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [414.56, 126.45, 447.88, 297.64],
+				]
+			],
+			9 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [297.64, 414.56, 468.83, 447.88],
+				]
+			],
+			10 => [
+				[
+					'uri' => 'https://www.setasign.com',
+					'rect' => [394.01, 297.64, 427.33, 468.83],
+				]
+			]
+		];
+
+		$reader = new PdfReader(new PdfParser(StreamReader::createByString($pdfString)));
+
+		for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
+			$this->compareExpectedLinks($pageNo, $expectedPageLinks[$pageNo], $reader);
+		}
+	}
+
+	/**
+	 * @copyright Copyright (c) Setasign GmbH & Co. KG (https://www.setasign.com)
+	 * @license http://opensource.org/licenses/mit-license The MIT License
+	*/
+	public function testExternalLinksWithDifferentPageBoundaries()
+	{
+		$mpdf = new Mpdf();
+		$mpdf->setSourceFile(__DIR__ . '/../data/pdfs/external-links/boxes.pdf');
+
+		$artBox = $mpdf->importPage(1, PageBoundaries::ART_BOX);
+		$trimBox = $mpdf->importPage(1, PageBoundaries::TRIM_BOX);
+		$bleedBox = $mpdf->importPage(1, PageBoundaries::BLEED_BOX);
+		$cropBox = $mpdf->importPage(1, PageBoundaries::CROP_BOX);
+
+		$mpdf->addPage('L');
+		$mpdf->useImportedPage($artBox, 0, 0, null, 160);
+		$mpdf->useImportedPage($trimBox, 70, 0, null, 160);
+		$mpdf->useImportedPage($bleedBox, 140, 0, null, 160);
+		$mpdf->useImportedPage($cropBox, 210, 0, null, 160);
+
+		$pdfString = $mpdf->OutputBinaryData();
+
+		$expectedLinks = [
+			[
+				'uri' => 'https://demos.setasign.com/?ArtBox',
+				'rect' => [2.67, 144.4, 42.7, 159.83],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?TrimBox',
+				'rect' => [-24.01, 117.73, 25.64, 133.15],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?BleedBox',
+				'rect' => [-50.69, 91.05, 6.4, 106.47],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?CropBox',
+				'rect' => [-77.37, 64.37, -25.48, 79.79]
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?ArtBox',
+				'rect' => [224.68, 167.99, 260.5, 181.79],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?TrimBox',
+				'rect' => [200.81, 144.12, 245.24, 157.92],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?BleedBox',
+				'rect' => [176.94, 120.25, 228.02, 134.05],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?CropBox',
+				'rect' => [153.07, 96.38, 199.5, 110.18]
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?ArtBox',
+				'rect' => [442.2, 187.09, 474.61, 199.57],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?TrimBox',
+				'rect' => [420.61, 165.49, 460.8, 177.98],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?BleedBox',
+				'rect' => [399.01, 143.9, 445.23, 156.38],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?CropBox',
+				'rect' => [377.41, 122.3, 419.42, 134.78]
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?ArtBox',
+				'rect' => [656.41, 202.87, 685.99, 214.26],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?TrimBox',
+				'rect' => [636.69, 183.15, 673.38, 194.54],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?BleedBox',
+				'rect' => [616.97, 163.43, 659.17, 174.83],
+			],
+			[
+				'uri' => 'https://demos.setasign.com/?CropBox',
+				'rect' => [597.25, 143.71, 635.6, 155.11]
+			]
+		];
+
+		$reader = new PdfReader(new PdfParser(StreamReader::createByString($pdfString)));
+		$this->compareExpectedLinks(1, $expectedLinks, $reader);
+	}
+
+	/**
+	 * @copyright Copyright (c) Setasign GmbH & Co. KG (https://www.setasign.com)
+	 * @license http://opensource.org/licenses/mit-license The MIT License
+	 */
+	public function testImportOfSpecialPageBoundaries()
+	{
+		$mpdf = new Mpdf();
+		$mpdf->setSourceFile(__DIR__ . '/../data/pdfs/external-links/[-1000 -1000 -500 -500].pdf');
+
+		$mpdf->AddPage();
+		$tplId = $mpdf->importPage(1);
+		$s = $mpdf->useTemplate($tplId, 20, 10, 150);
+		$mpdf->Rect(20, 10, 150, $s['height']);
+
+		$mpdf->setSourceFile(__DIR__ . '/../data/pdfs/external-links/[1000 500 -1000 -500]-R90.pdf');
+		$tplId = $mpdf->importPage(1);
+		$s = $mpdf->useTemplate($tplId, 20, 200, 50);
+		$mpdf->Rect(20, 200, 50, $s['height']);
+
+		$pdfString = $mpdf->OutputBinaryData();
+
+		$expectedLinks = [
+			[
+				'uri' => 'https://www.setasign.com',
+				'rect' => [141.73, 466.5, 514.43, 501.89],
+				'borderStyle' => [
+					'D' => [3],
+					'S' => 'D',
+					'W' => 1
+				],
+				'border' => [
+					0,
+					0,
+					1,
+					[3]
+				]
+			],
+			[
+				'uri' => 'https://www.setasign.com',
+				'rect' => [69.72, 191.01, 75.62, 260.79],
+				'borderStyle' => [
+					'D' => [3],
+					'S' => 'D',
+					'W' => 1
+				],
+				'border' => [
+					0,
+					0,
+					1,
+					[3]
+				]
+			],
+		];
+
+		$reader = new PdfReader(new PdfParser(StreamReader::createByString($pdfString)));
+		$this->compareExpectedLinks(1, $expectedLinks, $reader);
+	}
+
+	/**
+	 * @copyright Copyright (c) Setasign GmbH & Co. KG (https://www.setasign.com)
+	 * @license http://opensource.org/licenses/mit-license The MIT License
+	 */
+	public function testLinkInUtf16Encoding()
+	{
+		$mpdf = new Mpdf();
+		$mpdf->AddPage();
+		// This file has its link in UTF-16BE saved.
+		$mpdf->setSourceFile(__DIR__ . '/../data/pdfs/external-links/tuto6.pdf');
+		$tplId = $mpdf->importPage(2);
+		$mpdf->useTemplate($tplId);
+		$pdfString = $mpdf->OutputBinaryData();
+
+		$expectedLinks = [
+			[
+				// the strings are in UTF-16BE: http://pdf.wtf/ümlaut
+				'uri' => "\xFE\xFF\x00h\x00t\x00t\x00p\x00:\x00/\x00/\x00p\x00d\x00f\x00.\x00w\x00t\x00f\x00/\x00\xfc\x00m\x00l\x00a\x00u\x00t",
+				'rect' => [28.35, 749.82, 113.39, 807.87],
+			],
+			[
+				'uri' => "\xFE\xFF\x00h\x00t\x00t\x00p\x00:\x00/\x00/\x00p\x00d\x00f\x00.\x00w\x00t\x00f\x00/\x00\xfc\x00m\x00l\x00a\x00u\x00t",
+				'rect' => [387.18, 756.93, 468.87, 770.93],
+			],
+		];
+
+		$reader = new PdfReader(new PdfParser(StreamReader::createByString($pdfString)));
+		$this->compareExpectedLinks(1, $expectedLinks, $reader);
+	}
+
+	/**
+	 * Verify the generated PDF has the imported links on the correct page and in the correct position.
+	 *
+	 * This method comes from the FPDI library.
+	 *
+	 * @param int $pageNo
+	 * @param array $expectedLinks
+	 * @param PdfReader $reader
+	 * @param float $delta
+	 * @copyright Copyright (c) Setasign GmbH & Co. KG (https://www.setasign.com)
+	 * @license http://opensource.org/licenses/mit-license The MIT License
+	 */
+	protected function compareExpectedLinks($pageNo, array $expectedLinks, PdfReader $reader, $delta = 0.01)
+	{
+		$parser = $reader->getParser();
+		$pageDict = $reader->getPage($pageNo)->getPageDictionary();
+		$annots = PdfType::resolve($pageDict->value['Annots'], $parser);
+		$this->assertInstanceOf(PdfArray::class, $annots);
+
+		$this->assertCount(count($expectedLinks), $annots->value);
+
+		foreach ($expectedLinks as $idx => $linkData) {
+			$linkAnnotation = PdfType::resolve($annots->value[$idx], $parser);
+
+			$this->assertEquals($linkData['uri'], PdfString::unescape($linkAnnotation->value['A']->value['URI']->value));
+			$rect = Rectangle::byPdfArray($linkAnnotation->value['Rect'], $parser);
+			$rectValues = $rect->toArray();
+
+			$this->assertEqualsWithDelta($linkData['rect'], $rectValues, $delta, 'Rect @Page ' . $pageNo . '/' . $idx);
+
+			if (!isset($linkData['quadPoints'])) {
+				$this->assertFalse(isset($linkAnnotation->value['QuadPoints']));
+			} else {
+				$quadPoints = $this->dumpPdfType(PdfArray::ensure($linkAnnotation->value['QuadPoints'], count($linkData['quadPoints'])));
+				$this->assertEqualsWithDelta($linkData['quadPoints'], $quadPoints, $delta, 'QuadPoints @Page ' . $pageNo . '/' . $idx);
+			}
+
+			if (isset($linkData['f'])) {
+				$this->assertEquals(
+					$linkData['f'],
+					$this->dumpPdfType(PdfNumeric::ensure($linkAnnotation->value['F']))
+				);
+			}
+
+			if (isset($linkData['border'])) {
+				if ($linkData['border'] === false) {
+					$this->assertFalse(isset($linkAnnotation->value['Border']));
+				} else {
+					$this->assertEquals(
+						$linkData['border'],
+						$this->dumpPdfType(PdfArray::ensure($linkAnnotation->value['Border']))
+					);
+				}
+			}
+
+			if (isset($linkData['color'])) {
+				if ($linkData['color'] === false) {
+					$this->assertFalse(isset($linkAnnotation->value['C']));
+				} else {
+					$this->assertEqualsWithDelta(
+						$linkData['color'],
+						$this->dumpPdfType(PdfArray::ensure($linkAnnotation->value['C'])),
+						$delta,
+						''
+					);
+				}
+			}
+
+			if (isset($linkData['borderStyle'])) {
+				if ($linkData['borderStyle'] === false) {
+					$this->assertFalse(isset($linkAnnotation->value['BS']));
+				} else {
+					// we cannot compare the complete dictionary because the order may differ because TCPDF
+					// uses its own logic to create the BS entry
+					$bs = PdfDictionary::ensure($linkAnnotation->value['BS'])->value;
+					foreach ($linkData['borderStyle'] as $key => $value) {
+						$this->assertEquals($value, $this->dumpPdfType($bs[$key]));
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method comes from the FPDI library.
+	 *
+	 * @param PdfType $value
+	 * @return array|mixed|string
+	 * @copyright Copyright (c) Setasign GmbH & Co. KG (https://www.setasign.com)
+	 * @license http://opensource.org/licenses/mit-license The MIT License
+	 */
+	protected function dumpPdfType(PdfType $value)
+	{
+		switch (get_class($value)) {
+			case PdfName::class:
+			case PdfNumeric::class:
+			case PdfBoolean::class:
+				return $value->value;
+			case PdfString::class:
+				return PdfString::unescape($value->value);
+			case PdfArray::class:
+				$result = [];
+				foreach ($value->value as $entry) {
+					$result[] = $this->dumpPdfType($entry);
+				}
+				return $result;
+			case PdfDictionary::class:
+				$result = [];
+				foreach ($value->value as $key => $entry) {
+					$result[$key] = $this->dumpPdfType($entry);
+				}
+				return $result;
+			default:
+				throw new \InvalidArgumentException(
+					'Dump of PdfType "' . get_class($value) . '" is not implemented yet.'
+				);
+		}
 	}
 }
