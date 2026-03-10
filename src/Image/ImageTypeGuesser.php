d482671dd09c84c4243830670140bcdd0fce5429
Євген Хохлов <eugenkhokhlov@gmail.com>
2026-03-10 13:07:35 +0000

    avif support

--- src/Image/ImageTypeGuesser.php
+++ src/Image/ImageTypeGuesser.php
@@ -5,42 +5,49 @@ namespace Mpdf\Image;
 class ImageTypeGuesser
 {
 
-	/**
-	 * @param string $data
-	 *
-	 * @return null|string
-	 */
-	public function guess($data)
-	{
-		if (in_array(substr($data, 6, 4), ['JFIF', 'Exif'], true) || strpos($data, chr(255) . chr(216)) === 0) { // 0xFF 0xD8	// mpDF 5.7.2
-			return 'jpeg';
-		}
+    /**
+     * @param string $data
+     *
+     * @return null|string
+     */
+    public function guess($data, $file)
+    {
+        if (in_array(substr($data, 6, 4), ['JFIF', 'Exif'], true) || strpos($data, chr(255) . chr(216)) === 0) { // 0xFF 0xD8	// mpDF 5.7.2
+            return 'jpeg';
+        }
 
-		if (in_array(substr($data, 0, 4), ['RIFF'], true)) {
-			return 'webp';
-		}
+        if (in_array(substr($data, 0, 4), ['RIFF'], true)) {
+            return 'webp';
+        }
 
-		if (in_array(substr($data, 0, 6), ['GIF87a', 'GIF89a'], true)) {
-			return 'gif';
-		}
+        if (str_ends_with(strtolower($file), '.avif')) {
+            $imageInfoFromData = @getimagesize('data://application/octet-stream;base64,' . base64_encode($data));
+            if ($imageInfoFromData !== false && $imageInfoFromData[2] === IMAGETYPE_AVIF) {
+                return 'avif';
+            }
+        }
 
-		if (strpos($data, chr(137) . 'PNG' . chr(13) . chr(10) . chr(26) . chr(10)) === 0) {
-			return 'png';
-		}
+        if (in_array(substr($data, 0, 6), ['GIF87a', 'GIF89a'], true)) {
+            return 'gif';
+        }
 
-		if (strpos($data, chr(215) . chr(205) . chr(198) . chr(154)) === 0) {
-			return 'wmf';
-		}
+        if (strpos($data, chr(137) . 'PNG' . chr(13) . chr(10) . chr(26) . chr(10)) === 0) {
+            return 'png';
+        }
 
-		if (preg_match('/<svg.*<\/svg>/is', $data)) {
-			return 'svg';
-		}
+        if (strpos($data, chr(215) . chr(205) . chr(198) . chr(154)) === 0) {
+            return 'wmf';
+        }
 
-		if (strpos($data, 'BM') === 0) {
-			return 'bmp';
-		}
+        if (preg_match('/<svg.*<\/svg>/is', $data)) {
+            return 'svg';
+        }
 
-		return null;
-	}
+        if (strpos($data, 'BM') === 0) {
+            return 'bmp';
+        }
+
+        return null;
+    }
 
 }
