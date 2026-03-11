3979289947f16afcb57bd9b27971a014beabcadf
Matej Humpal <finwe@finwe.info>
2026-03-11 11:59:54 +0100

    Update version constant

--- src/Mpdf.php
+++ src/Mpdf.php
@@ -33,7 +33,7 @@ class Mpdf implements \Psr\Log\LoggerAwareInterface
 	use FpdiTrait;
 	use MpdfPsrLogAwareTrait;
 
-	const VERSION = '8.3.1';
+	const VERSION = '8.3.2';
 
 	const SCALE = 72 / 25.4;
 
