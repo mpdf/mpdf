4b9dfbb2fd4535c61ba63e43d7ba388bf4f93f70
brandonwilsondmv-cmyk <brandon.wilson.dmv@gmail.com>
2026-08-28 12:18:58 -0400

    Create index.php

---
+++ tests/index.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mpdf;
+
+/**
+ * Setup our unit testing functionality
+ *
+ * @package    mPDF
+ * @author     Blue Liquid Designs <admin@blueliquiddesigns.com.au>
+ * @copyright  2015 Blue Liquid Designs
+ * @license    GPLv2
+ * @since      GPL-2.0
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Create a new instance of the mPDF class
+// We do this here to force the autoloader to include the actual file and its constants
+// It means tests will have access to all of mPDF's constants without first creating a new instance (and everything is loaded)
+new Mpdf();
+
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->WriteHTML('<h1>Hello world!</h1>');
+$mpdf->Output();
